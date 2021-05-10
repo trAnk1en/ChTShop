10fda3967ad94059051a1f3ebcd943dd3b00af30
trAnk1en <73920532+trAnk1en@users.noreply.github.com>
2021-05-10 09:27:30 +0700

    Add files via upload

---
+++ ChTShop/configs/config_DB.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clouddb: {
+        urldb : 'mongodb+srv://trungshin_6969:QK3xrnkUn8MUUBUw@cluster0.krj6n.mongodb.net/shopsmall?retryWrites=true&w=majority',
+        dbname : "shopsmall",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
+
